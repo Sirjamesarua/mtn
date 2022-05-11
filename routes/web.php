e2f6deef6802c37d75b91e47b0023bc2a11b17d6
sirjamesarua <dev.sirjames@gmail.com>
2022-05-11 20:50:12 +0530

    setup

--- routes/web.php
+++ routes/web.php
@@ -45,10 +45,36 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/verify-payment/{reference}', [App\Http\Controllers\Auth\HomeController::class, 'verify']);
+//Route::get('/verify-payment/{reference}', [App\Http\Controllers\Auth\HomeController::class, 'verify']);
 
-/*Route::get('/verify-payment/{reference}', function(){
-	echo '<script>
-		console.log("hghgh");
-	</script>';
-});*/
+Route::get('/verify-payment/{reference}', function(){
+	$curl = curl_init();
+         $sec="sk_test_c6804e7ad469d1e972bb693b13e25018a09d3cc2";
+          
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.paystack.co/transaction/verify/:reference",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+              "Authorization: Bearer $sec",
+              "Cache-Control: no-cache",
+            ),
+          ));
+          
+          $response = curl_exec($curl);
+          $err = curl_error($curl);
+          //curl_close($curl);
+          
+          /*if ($err) {
+            echo "cURL Error #:" . $err;
+          } else {
+            echo $response;
+          }*/
+          return $response;
+          //$new_data = json_decode($response);
+          //return $new_data;
+});
