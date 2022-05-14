8d66879c2e002009f7c92a00d0512d435f8b8fa0
sirjamesarua <dev.sirjames@gmail.com>
2022-05-14 05:57:23 +0530

    setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -360,8 +360,8 @@ function payWithPaystack(e) {
 
 
         callback: function(response){
-            let amountpaid =document.getElementById("amount").val();
-            let refeerer_no =document.getElementById("refereer-number").val();
+            //let amountpaid =document.getElementById("amount").val();
+           // let refeerer_no =document.getElementById("refereer-number").val();
           $.ajax({
             url:  "/verify-payment/"+response.reference,
             type: 'GET',
