b6973b1c6d8eb230ebf4503d3c99d4060db62859
sirjamesarua <dev.sirjames@gmail.com>
2022-05-14 20:00:53 +0530

    setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -235,194 +235,188 @@
 </div>
 
 <script>
-var token='{{csrf_token()}}';
-var amountpaid = $("input#amount").val();
-var refeerer_no = $("input#refereer-number").val();
-/*SLIDER*/
-var slideIndex = 0; 
-carousel();
+$(document).ready(function(){
+    var token='{{csrf_token()}}';
+    var amountpaid = $("input#amount").val();
+    var refeerer_no = $("input#refereer-number").val();
+    /*SLIDER*/
+    var slideIndex = 0; 
+    carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1;} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 5000); 
-}
+    function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+      }
+      slideIndex++;
+      if (slideIndex > x.length) {slideIndex = 1;} 
+      x[slideIndex-1].style.display = "block"; 
+      setTimeout(carousel, 5000); 
+    }
 
-/*MENU CLOSEBTN*/
-function closebtn(){
-    var x= document.getElementById("sidenav");
-    x.style.display="none";
-}
+    /*MENU CLOSEBTN*/
+    function closebtn(){
+        var x= document.getElementById("sidenav");
+        x.style.display="none";
+    }
 
-/*MENU OPENBTN*/
-function openbtn(){
-    var x= document.getElementById("sidenav");
-    x.style.display="block";
-}
+    /*MENU OPENBTN*/
+    function openbtn(){
+        var x= document.getElementById("sidenav");
+        x.style.display="block";
+    }
 
-/*REGISTER BUTTON*/
-function registerbtn(){
-    var xx= document.getElementById("loginform");
-    var yy= document.getElementById("loginbtn");
-    var x= document.getElementById("form");
-    var y= document.getElementById("registerbtn");
-    var loginbtn= document.getElementById("loginbtn");
-    loginbtn.style.display="none";
-    xx.style.display="none";
-    yy.style.display="none";
-    x.style.display="block";
-    y.style.display="none";
-}
+    /*REGISTER BUTTON*/
+    function registerbtn(){
+        var xx= document.getElementById("loginform");
+        var yy= document.getElementById("loginbtn");
+        var x= document.getElementById("form");
+        var y= document.getElementById("registerbtn");
+        var loginbtn= document.getElementById("loginbtn");
+        loginbtn.style.display="none";
+        xx.style.display="none";
+        yy.style.display="none";
+        x.style.display="block";
+        y.style.display="none";
+    }
 
-/*LOGIN BUTTON*/
-function loginbtn(){
-    var xx= document.getElementById("form");
-    var yy= document.getElementById("registerbtn");
-    var x= document.getElementById("loginform");
-    var y= document.getElementById("loginbtn");
-    var registerbtn= document.getElementById("registerbtn");
-    registerbtn.style.display="none";
-    xx.style.display="none";
-    yy.style.display="none";
-    x.style.display="block";
-    y.style.display="none";
-}
+    /*LOGIN BUTTON*/
+    function loginbtn(){
+        var xx= document.getElementById("form");
+        var yy= document.getElementById("registerbtn");
+        var x= document.getElementById("loginform");
+        var y= document.getElementById("loginbtn");
+        var registerbtn= document.getElementById("registerbtn");
+        registerbtn.style.display="none";
+        xx.style.display="none";
+        yy.style.display="none";
+        x.style.display="block";
+        y.style.display="none";
+    }
 
-/*NEXTSTEP*/
-var loginnumber = $("input#loginnumber");
-var loginpassword = $("input#loginpassword");
-var loginpassword2 = $("input#loginpassword2");
-//var loginpasswordcomfirm = $("input#loginpasswordcomfirm");
+    /*NEXTSTEP*/
+    var loginnumber = $("input#loginnumber");
+    var loginpassword = $("input#loginpassword");
+    var loginpassword2 = $("input#loginpassword2");
+    //var loginpasswordcomfirm = $("input#loginpasswordcomfirm");
 
-        
+            
 
-function nextstep(){
-        document.getElementById("nextstepbtn").style.display="none";
-        document.getElementById("secondstep").style.display="block";
-}
+    function nextstep(){
+            document.getElementById("nextstepbtn").style.display="none";
+            document.getElementById("secondstep").style.display="block";
+    }
 
-/*PHONR NUMBER VALIDATION*/
-function phonecheck(){
-    var form=document.form;
-    var number=form.elements["number"].value;
-    if( number=="08140480701"){
-        document.getElementById("output").innerHTML("<b>Correct</b>");
-        };
-}
+    /*PHONR NUMBER VALIDATION*/
+    function phonecheck(){
+        var form=document.form;
+        var number=form.elements["number"].value;
+        if( number=="08140480701"){
+            document.getElementById("output").innerHTML("<b>Correct</b>");
+            };
+    }
 
-function copyrefeererlink(){
-    var copytext = $("#invitelink").val();
+    function copyrefeererlink(){
+        var copytext = $("#invitelink").val();
 
-    //navigator.clipboard.writeText(copytext);
+        //navigator.clipboard.writeText(copytext);
 
-    alert(copytext);
-}
+        alert(copytext);
+    }
 
-//paystack
-const paymentForm = document.getElementById('paymentForm');
-paymentForm.addEventListener("submit", payWithPaystack, false);
-function payWithPaystack(e) {
-  e.preventDefault();
-  let handler = PaystackPop.setup({
-    key: 'pk_test_6fa4bde5ef3fed310ae776d4ea59b9e3e8b203a4', // Replace with your public key
-    email: document.getElementById("email-address").value,
-    amount: document.getElementById("amount").value * 100,
-    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-    // label: "Optional string that replaces customer email"
-    onClose: function(){
-      alert('Window closed.');
-    },
+    //paystack
+    const paymentForm = document.getElementById('paymentForm');
+    paymentForm.addEventListener("submit", payWithPaystack, false);
+    function payWithPaystack(e) {
+      e.preventDefault();
+      let handler = PaystackPop.setup({
+        key: 'pk_test_6fa4bde5ef3fed310ae776d4ea59b9e3e8b203a4', // Replace with your public key
+        email: document.getElementById("email-address").value,
+        amount: document.getElementById("amount").value * 100,
+        ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+        // label: "Optional string that replaces customer email"
+        onClose: function(){
+          alert('Window closed.');
+        },
 
-    /*callback: function(response){
-      //let message = 'Payment complete! Reference: ' + response.reference;
-      //alert(message);
-      //alert("/verify-payment/"+response.reference);
-      $.ajax({
-        type:"GET",
-        //url: "{{URL::to ('verify-payment')}}/"+reference,
-        url: "/verify-payment/"+response.reference,
-        success: function(comfirmm){
-            alert(comfirmm);
-        //success: function(response){
-            //console.log(response);
-            //if(response[0].status = true){
-              //  $('form').prepend('<h2>${response[0].message}</h2>');
-            //}else{
-             //   $('form').prepend('<h2>failed to verify payment</h2>');
-           // }
-        }
-      })
-    }*/
-
-
-        callback: function(response){
+        /*callback: function(response){
+          //let message = 'Payment complete! Reference: ' + response.reference;
+          //alert(message);
+          //alert("/verify-payment/"+response.reference);
           $.ajax({
-            url:  "/verify-payment/"+response.reference,
-            type: 'GET',
-            success: function (response) {
-                    console.log("Correctsuces");
-                if(response.status = true){
-                    console.log("Correct1");
-                    alert(amountpaid);
-                    //window.location.replace("/paystack/save");
+            type:"GET",
+            //url: "{{URL::to ('verify-payment')}}/"+reference,
+            url: "/verify-payment/"+response.reference,
+            success: function(comfirmm){
+                alert(comfirmm);
+            //success: function(response){
+                //console.log(response);
+                //if(response[0].status = true){
+                  //  $('form').prepend('<h2>${response[0].message}</h2>');
+                //}else{
+                 //   $('form').prepend('<h2>failed to verify payment</h2>');
+               // }
+            }
+          })
+        }*/
 
-                        $.ajaxSetup({
-                            headers:{
-                                'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content'),
-                            }
-                        });
 
-                          $.ajax({
-                            type: 'POST',
-                            url:  "/paystack/save",
-                            data:{
-                                '_token': token,
-                                amountpaid:amountpaid,
-                                refeerer_no:refeerer_no,
-                            },
-                            success: function (response) {
-                            console.log(amountpaid);
-                            console.log("Correctsuces2");
-                                    if(response.status==200){
-                                        window.location.replace("/home");
-                                        console.log("Correct2");
-                                        console.log(response.amount);
-                                    }
-                            }
-                          });
-                    console.log("endCorrectsuces");
+            callback: function(response){
+              $.ajax({
+                url:  "/verify-payment/"+response.reference,
+                type: 'GET',
+                success: function (response) {
+                        console.log("Correctsuces");
+                    if(response.status = true){
+                        console.log("Correct1");
+                        alert(amountpaid);
+                        //window.location.replace("/paystack/save");
 
-                } else{
-                    alert("payment not successful");
+                            $.ajaxSetup({
+                                headers:{
+                                    'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content'),
+                                }
+                            });
+
+                              $.ajax({
+                                type: 'POST',
+                                url:  "/paystack/save",
+                                data:{
+                                    '_token': token,
+                                    amountpaid:amountpaid,
+                                    refeerer_no:refeerer_no,
+                                },
+                                success: function (response) {
+                                console.log(amountpaid);
+                                console.log("Correctsuces2");
+                                        if(response.status==200){
+                                            window.location.replace("/home");
+                                            console.log("Correct2");
+                                            console.log(response.amount);
+                                        }
+                                }
+                              });
+                        console.log("endCorrectsuces");
+
+                    } else{
+                        alert("payment not successful");
+                    }
+                  
+
                 }
-              
+              });
 
             }
-          });
-
-        }
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-  });
-  handler.openIframe();
+      });
+      handler.openIframe();
+    }
 }
 </script>
                     
