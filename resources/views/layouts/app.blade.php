0153a8ec9b44278cd76761944a74ea1888bfdcda
sirjamesarua <dev.sirjames@gmail.com>
2022-05-15 10:03:30 +0530

    setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -238,7 +238,6 @@
     var token='{{csrf_token()}}';
     var amountpaid = $("input.amountpaid").val();
     var refeerer_no = $("input#refereer-number").val();
-    alert(amountpaid);
     /*SLIDER*/
     var slideIndex = 0; 
     carousel();
@@ -324,6 +323,15 @@
 
         alert(copytext);
     }
+
+
+
+
+$(document).ready(function(){
+    alert(amountpaid);
+});
+
+
 
     //paystack
     const paymentForm = document.getElementById('paymentForm');
