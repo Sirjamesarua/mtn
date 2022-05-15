d174c9d56157eeff2168522c96925d46ae16a550
sirjamesarua <dev.sirjames@gmail.com>
2022-05-15 11:12:30 +0530

    setup

--- resources/views/paystack.blade.php
+++ resources/views/paystack.blade.php
@@ -56,9 +56,10 @@
                               <input type="number" id="refereer-number" />
                             </div>
                             <br />
-                            <div class="form-submit">
-                              <button  class="w3-text-white w3-blue submit w3-center w3-border-large w3-round-xxlarge w3-jumbo" onclick="payWithPaystack()" style="width: 40%;"> Pay </button>
-                            </div>
+                            <button onclick="payWithPaystack()">hh</button>
+                            <!--<div class="form-submit">
+                              <button type="submit" class="w3-text-white w3-blue submit w3-center w3-border-large w3-round-xxlarge w3-jumbo" onclick="payWithPaystack(event)" style="width: 40%;"> Pay </button>
+                            </div>-->
 
 
 
